70cea8bc3a4138ff47c979926e0d2099be07f6e7
antoniusmisfit <antoniusmisfit@gmail.com>
2016-04-26 00:12:43 -0400

    Added mobile support for htmlwrap

--- htmlwrap.sh
+++ htmlwrap.sh
@@ -47,6 +47,8 @@ cat << EOF
 <!doctype html>
 <html>
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>$title</title>
 <style>
 html{font-family: "sans";background-color: white;color: black}
